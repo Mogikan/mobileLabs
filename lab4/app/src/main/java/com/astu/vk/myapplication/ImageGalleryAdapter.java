482519e6e11a6417161a262de39b6ae8aa4a9c52
Mogikan <gcnew@yandex.ru>
2017-09-29 13:04:31 +0700

    l4 beginning

---
+++ lab4/app/src/main/java/com/astu/vk/myapplication/ImageGalleryAdapter.java
@@ -0,0 +1,26 @@
+package com.astu.vk.myapplication;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by VK on 9/28/2017.
+ */
+
+public class ImageGalleryAdapter extends RecyclerView.Adapter<ImageCardViewHolder> {
+    @Override
+    public ImageCardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ImageCardViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
